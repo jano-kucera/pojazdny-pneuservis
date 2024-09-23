aec9dfeedc9f49ecb1a8974e35d564b0fce990a1
jano-kucera <janislavkucera@gmail.com>
2024-09-23 19:12:05 +0200

    Auto-generated commit

--- src/app/contact/app.contact.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core"
-
-@Component({
-    selector: "app-contact",
-    standalone: true,
-    styleUrl: "./app.contact.scss",
-    templateUrl: "./app.contact.html"
-})
-export class AppContactComponent { }
