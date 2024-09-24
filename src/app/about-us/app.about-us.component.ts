69a5f0e1494d76548af378c76bd8da7f2d556aa5
jano-kucera <janislavkucera@gmail.com>
2024-09-24 22:57:04 +0200

    Overhaul of the about us component

--- src/app/about-us/app.about-us.component.ts
+++ src/app/about-us/app.about-us.component.ts
@@ -1,10 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
+    imports: [
+        MatButtonModule,
+        MatExpansionModule,
+        MatListModule,
+    ],
     selector: 'app-about-us',
     standalone: true,
     styleUrl: './app.about-us.component.scss',
     templateUrl: './app.about-us.component.html'
 })
-
-export class AppAboutUsComponent { }
+export class AppAboutUsComponent {
+    viewportScroller = inject(ViewportScroller);
+}
