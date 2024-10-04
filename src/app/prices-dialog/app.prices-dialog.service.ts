2708ea577f98918c13a703f63ec685e1bd03dfb2
jano-kucera <janislavkucera@gmail.com>
2024-10-04 20:12:24 +0200

    Fix modal theme

--- src/app/prices-dialog/app.prices-dialog.service.ts
+++ src/app/prices-dialog/app.prices-dialog.service.ts
@@ -1,5 +1,6 @@
 import { inject, Injectable } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
+import { AppThemeService } from "../app.theme.service";
 import { AppPricesDialogComponent } from "./app.prices-dialog.component";
 
 /**
@@ -10,6 +11,9 @@ export class AppPricesDialogService {
     /** Angular material dialog service. */
     private dialog: MatDialog = inject(MatDialog);
 
+    /** App's theme service. */
+    private appThemeService: AppThemeService = inject(AppThemeService);
+
     /**
      * Opens prices dialog.
      */
@@ -17,6 +21,9 @@ export class AppPricesDialogService {
         this.dialog.open(AppPricesDialogComponent, {
             enterAnimationDuration: 200,
             maxWidth: "1000px",
+            panelClass: this.appThemeService.lightTheme
+                ? "light-theme"
+                : "dark-theme",
         });
     }
 }
